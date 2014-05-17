9489d0313787c49ea3248922c8c22636b923b659
robcg08 <robcg08@hotmail.com>
2014-05-16 23:00:37 -0600

    Registro Personas PHP

---
+++ Interfaz/Registros/registroPersonas.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf8">
+        <title>Registro de Personas</title>
+        <link href="/funes/Interfaz/CSS/registroPersonas.css" type="text/css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Patua+One' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Oxygen:300,400' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <section>
+            <div class="containerBody">
+                <div class="mainBody">
+                    <div class="divRegistro">
+                        <h1>Registro de Personas</h1>
+                        <div class="divForm">
+                            <form class="formRegistroPersonas">
+                                <div>
+                                    <p>Nombre</p>
+                                    <input type="text">
+                                </div>
+                                <div>
+                                    <p>Primer Apellido</p>
+                                    <input type="text">
+                                </div>
+                                <div>
+                                    <p>Segundo Apellido</p>
+                                    <input type="text">
+                                </div>
+                                <div>
+                                    <p>Género</p>
+                                    <select>
+                                        <option>M</option>
+                                        <option>F</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <p>Fecha de Nacimiento</p>
+                                    <input type="date">
+                                </div>
+                                <div>
+                                    <p>Edad</p>
+                                    <input type="text" readonly>
+                                </div>
+                                <div>
+                                    <p>Institución donde labora</p>
+                                    <input type="text">
+                                </div>
+                                <div>
+                                    <p>Cargo que desempeña</p>
+                                    <input type="text">
+                                </div>
+                                <div class="submit">
+                                    <input type="submit" class="btnRegistrar" value="Registrar">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+    <footer>
+
+    </footer>
+</html>
