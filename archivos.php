d3c7fdf4a34281ba6efbdec46093bb99e425c236
Benjamin-Lewis <blewism29@gmail.com>
2014-05-18 16:11:40 -0600

    archivos.php
    .PHP para subir archivos de evidencias

---
+++ archivos.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>    
+    <body>
+        <form name = "paci" action="uploader1.php" method = "post" enctype='multipart/form-data' class="letras_formulario">
+        <input type="file" name="uploaded_file1" id="uploaded_file1">
+        <div>
+            <input type="submit" value="Send">
+        </div>
+        </form> 
+    </body>
+</html>
