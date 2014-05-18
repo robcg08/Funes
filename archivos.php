a46ee57de42fc7deebb555b17aa043ae4c348a24
Benjamin-Lewis <blewism29@gmail.com>
2014-05-18 16:16:57 -0600

    archivos.php
    cambio para que el el submit lo envie al archivo archivos2.php

--- archivos.php
+++ archivos.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>    
     <body>
-        <form name = "paci" action="uploader1.php" method = "post" enctype='multipart/form-data' class="letras_formulario">
+        <form name = "paci" action="archivos2.php" method = "post" enctype='multipart/form-data' class="letras_formulario">
         <input type="file" name="uploaded_file1" id="uploaded_file1">
         <div>
             <input type="submit" value="Send">
