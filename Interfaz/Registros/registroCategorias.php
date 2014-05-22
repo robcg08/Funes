765320aa71babf95b16f75a73ebf2f952b033684
robcg08 <robcg08@hotmail.com>
2014-05-21 22:04:53 -0600

    registro categorias PHP

---
+++ Interfaz/Registros/registroCategorias.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Registro de Categorías</title>
+    <link href="/funes/Interfaz/CSS/registroCategorias.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Patua+One' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oxygen:300,400' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <body>
+        <div class="containerBody">
+            <div class="mainBody">
+                <div class="divRegistro">
+                    <h1>Registro de Categorías</h1>
+                    <div class="divForm">
+                        <form class="formRegistroCategorias">
+                            <div>
+                                <p>Nombre</p>
+                                <input type="text">
+                            </div>
+                            <div class="submit">
+                                <input type="submit" class="btnRegistrar" value="Registrar">
+                            </div>
+                        </form>
+                        <div class="categorias">
+                            <h1>Categorías Registradas</h1>
+                        </div>
+                        <!-- mostrar una tabla con las categorias registradas -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</body>
+
+</html>
