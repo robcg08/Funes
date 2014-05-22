179f427af1ab4d12d6eb614e75685830b15d8fb8
robcg08 <robcg08@hotmail.com>
2014-05-21 19:50:45 -0600

    registro entidades PHP

---
+++ Interfaz/Registros/registroEntidades.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Registro de Entidades</title>
+    <link href="/funes/Interfaz/CSS/registroEntidades.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Patua+One' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oxygen:300,400' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <section>
+        <div class="containerBody">
+            <div class="mainBody">
+                <div class="divRegistro">
+                    <h1>Registro de Entidades</h1>
+                    <div class="divForm">
+                        <form class="formRegistroEntidades">
+                            <div>
+                                <p>Nombre</p>
+                                <input type="text">
+                            </div>
+                            <div>
+                                <p>Cédula Jurídica</p>
+                                <input type="text">
+                            </div>
+                            <div class="dir">
+                                <h2>Dirección</h2>
+                            </div>
+                            <div>
+                                <p>País</p>
+                                <select></select>
+                                <div class="prov">
+                                    <div class="provincia">
+                                        <p>Provincia</p>
+                                        <select></select>
+                                    </div>
+                                    <div class="canton">
+                                        <p>Cantón</p>
+                                        <select></select>
+                                    </div>
+                                </div>
+                                <div class="dis">
+                                    <div class="distrito">
+                                        <p>Distrito</p>
+                                        <select></select>
+                                    </div>
+                                    <div class="barrio">
+                                        <p>Barrio</p>
+                                        <select></select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="submit">
+                                <input type="submit" class="btnRegistrar" value="Registrar">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
