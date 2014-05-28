66ea2d96407c5b9f32fe484963257aa39051d032
Sleyter A <sley9468@hotmail.com>
2014-05-28 00:59:24 -0600

    Prueba

--- Interfaz/head/head.php
+++ Interfaz/head/head.php
@@ -5,14 +5,14 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Mi Perfil</a></li>
-            </ul>
-            <ul>
-            <form class="navbar-form navbar-left" role="search">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">Buscar</button>
-            </form>
+                <li>
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Buscar</button>
+                    </form>
+                </li>
             </ul>
         </div>
     </div>
