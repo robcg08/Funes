11c76ecdc1cb633b13a9c83cc5db4b395db2a77f
robcg08 <robcg08@hotmail.com>
2014-06-02 15:53:45 -0600

    Consultas

---
+++ Interfaz/Consultas/consultas.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Consultas</title>
+    <link href='http://fonts.googleapis.com/css?family=Patua+One' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oxygen:300,400' rel='stylesheet' type='text/css'>
+    <link href="../CSS/consultas.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <section>
+        <div class="containerBody">
+            <div class="mainBody">
+                <div class="divRegistro">
+                    <div class="divForm">
+                        <form class="formConsultas">
+                            <div class="nom">
+                                <p>Nombre</p>
+                                <input type="text">
+                            </div>
+                            <div class="tipo">
+                                <select></select>
+                            </div>
+
+                            <div class="submit">
+                                <input type="submit" class="btnConsultas" value="Consultar">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
